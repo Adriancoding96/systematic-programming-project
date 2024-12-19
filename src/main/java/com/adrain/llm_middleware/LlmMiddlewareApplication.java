c49439810453b23092bb820143264447ce2dbeb3
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-19 13:11:22 +0000

    First commit ***Added*** general project structure

---
+++ src/main/java/com/adrain/llm_middleware/LlmMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.adrain.llm_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrain/llm_middleware/LlmMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrain.llm_middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
