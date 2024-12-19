30f562edddd88c992caa02df212bb8514e11ddda
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-19 15:59:56 +0000

    ***Added*** UserService interface defining methods for service implementation

---
+++ src/main/java/com/adrain/llm_middleware/service/UserService.java
@@ -0,0 +1,9 @@
+package com.adrain.llm_middleware.service;
+
+import com.adrain.llm_middleware.model.User;
+import com.adrain.llm_middleware.record.auth.SignupRequest;
+
+public interface UserService {
+  User getUserByEmail(String email);
+  void signUp(SignupRequest signupRequest);
+}
