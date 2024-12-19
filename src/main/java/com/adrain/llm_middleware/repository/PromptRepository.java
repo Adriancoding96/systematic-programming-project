cbf5a1e49a9d26150dd3e210e63207c9d00a3efa
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-19 21:52:12 +0000

    **Added** PromptRepository for interacting with prompt table in database

---
+++ src/main/java/com/adrain/llm_middleware/repository/PromptRepository.java
@@ -0,0 +1,10 @@
+package com.adrain.llm_middleware.repository;
+
+import com.adrain.llm_middleware.model.Prompt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromptRepository extends JpaRepository<Prompt, Long> {
+
+  
+}
