5ed74c8681e95a04ec43c3e9036f3695d90d5da1
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-19 20:40:19 +0000

    ***Changed*** changed table name of user model from user -> users temporarily because naming conflict with h2

--- src/main/java/com/adrain/llm_middleware/model/User.java
+++ src/main/java/com/adrain/llm_middleware/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,6 +19,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Table(name = "users") //Temporary while using h2-database
 public class User {
   
   @Id
