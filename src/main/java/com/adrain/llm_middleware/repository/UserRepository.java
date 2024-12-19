29ec9d0f3960f7399874f9b207fdeae24a028209
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-19 16:15:38 +0000

    ***Added*** UserRepositroy for interaction with user table in database

---
+++ src/main/java/com/adrain/llm_middleware/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.adrain.llm_middleware.repository;
+
+import java.util.Optional;
+
+import com.adrain.llm_middleware.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email); 
+}
