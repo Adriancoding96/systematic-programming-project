d934f5df231cb54ae699765a6c613471a7f6c6ff
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-21 15:13:19 +0000

    **Added** mock api key resource on api context test class

--- src/test/java/com/adrain/llm_middleware/LlmMiddlewareApplicationTests.java
+++ src/test/java/com/adrain/llm_middleware/LlmMiddlewareApplicationTests.java
@@ -1,8 +1,10 @@
 package com.adrain.llm_middleware;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"api.key=TEST_API_KEY"})
 class LlmMiddlewareApplicationTests {
 
 	@Test
