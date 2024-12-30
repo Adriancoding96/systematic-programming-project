0cca7fb0920c4848ebeb4b72f5236d9cbd149259
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2024-12-30 10:35:00 +0000

    **Added** method in prompt service to extract and return first available response from openai api reponse record

--- src/main/java/com/adrain/llm_middleware/service/PromptService.java
+++ src/main/java/com/adrain/llm_middleware/service/PromptService.java
@@ -1,8 +1,12 @@
 package com.adrain.llm_middleware.service;
 
+import com.adrain.llm_middleware.record.Prompt.PromptRequest;
+import com.adrain.llm_middleware.record.Prompt.PromptResponse;
 import com.adrain.llm_middleware.record.api.OpenAiResponse;
 
 public interface PromptService {
+
+  PromptResponse newPrompt(PromptRequest request);
 
   OpenAiResponse getResponse(String prompt);
   
