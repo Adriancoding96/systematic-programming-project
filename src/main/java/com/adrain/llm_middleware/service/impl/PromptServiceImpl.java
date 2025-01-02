2691db636849c0dcea3d595987ec396fc9013374
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-02 20:51:00 +0000

    **Changed** new prompt method in prompt service impl to construct of list of keywords from openai api response and return it in promp response record

--- src/main/java/com/adrain/llm_middleware/service/impl/PromptServiceImpl.java
+++ src/main/java/com/adrain/llm_middleware/service/impl/PromptServiceImpl.java
@@ -3,11 +3,12 @@ package com.adrain.llm_middleware.service.impl;
 import java.util.List;
 
 import com.adrain.llm_middleware.api.OpenAiClient;
-import com.adrain.llm_middleware.record.Prompt.PromptRequest;
-import com.adrain.llm_middleware.record.Prompt.PromptResponse;
 import com.adrain.llm_middleware.record.api.OpenAiResponse;
+import com.adrain.llm_middleware.record.prompt.PromptRequest;
+import com.adrain.llm_middleware.record.prompt.PromptResponse;
 import com.adrain.llm_middleware.repository.PromptRepository;
 import com.adrain.llm_middleware.service.PromptService;
+import com.adrain.llm_middleware.util.KeywordSearcher;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,23 +20,26 @@ public class PromptServiceImpl implements PromptService {
 
   private final PromptRepository promptRepository;
   private final OpenAiClient openAiClient;
+  private final KeywordSearcher keywordSearcher;
 
   @Autowired
   public PromptServiceImpl(PromptRepository promptRepository, OpenAiClient openAiClient) {
     this.promptRepository = promptRepository;
     this.openAiClient = openAiClient;
+    this.keywordSearcher = KeywordSearcher.getInstance();
   }
 
   /*
    * Method extracts response from llm api and returns it to PromptController
    *
    * @param request: record that contains prompt text
-   * @return response: returns record that contains response text
+   * @return response: returns record that contains response text and keywords found in response
    * */
   @Override
   public PromptResponse newPrompt(PromptRequest request) {
     OpenAiResponse fullResponse = getResponse(request.prompt());
-    return new PromptResponse(fullResponse.choices().get(0).message().toString());
+    List<String> keywords = keywordSearcher.getKeywords(fullResponse.choices().get(0).message().content());
+    return new PromptResponse(fullResponse.choices().get(0).message().content(), keywords);
      
   }
 
@@ -50,140 +54,4 @@ public class PromptServiceImpl implements PromptService {
     return monoResponse.block();
   }
 
-  // Create method to extract meta data from api response, words of interest are following, programming languages, packages, frameworks, databases, third party technologies
-  private final List<String> KEYWORDS = List.of(
-    "java",
-    "rust",
-    "c",
-    "cpp",
-    "c++",
-    "c#",
-    "javascript",
-    "typescript",
-    "python",
-    "bash",
-    "ruby",
-    "php",
-    "sql",
-    "kotlin",
-    "go",
-    "swift",
-    "r",
-    "dart",
-    "scala",
-    "perl",
-    "lua",
-    "objective-c",
-    "powershell",
-    "basic",
-    "haskell",
-    "elixir",
-    "f#",
-    "ocaml",
-    "vimscript",
-    "odin",
-    "zig",
-    "assembly",
-    "markdown",
-    "html",
-    "css",
-    "cobol",
-    "matlab",
-    "dreamberd",
-    "holyc",
-    "spring",
-    "hibernate",
-    "jakarta-ee",
-    "actix",
-    "rocket",
-    "qt",
-    "boost",
-    "dotnet",
-    "asp.net",
-    "react",
-    "angular",
-    "vue",
-    "svelte",
-    "next.js",
-    "nuxt.js",
-    "express",
-    "nest.js",
-    "django",
-    "flask",
-    "fastapi",
-    "pyramid",
-    "rails",
-    "sinatra",
-    "laravel",
-    "symfony",
-    "codeigniter",
-    "cakephp",
-    "ktor",
-    "gin",
-    "echo",
-    "fiber",
-    "vapor",
-    "kitura",
-    "perfect",
-    "shiny",
-    "plumber",
-    "flutter",
-    "play",
-    "akka",
-    "mojolicious",
-    "catalyst",
-    "dancer",
-    "luvit",
-    "openresty",
-    "cocoa",
-    "psframework",
-    "yesod",
-    "snap",
-    "phoenix",
-    "suave",
-    "giraffe",
-    "ocaml-lwt",
-    "mysql",
-    "postgresql",
-    "oracle",
-    "mssql",
-    "mongodb",
-    "redis",
-    "cassandra",
-    "mariadb",
-    "sqlite",
-    "elasticsearch",
-    "postman",
-    "trello",
-    "slack",
-    "jira",
-    "confluence",
-    "github",
-    "gitlab",
-    "bitbucket",
-    "docker",
-    "kubernetes",
-    "terraform",
-    "jenkins",
-    "aws",
-    "azure",
-    "google cloud",
-    "ibm cloud",
-    "oracle cloud",
-    "digitalocean",
-    "s3",
-    "ec2",
-    "ecs",
-    "lambda",
-    "fargate",
-    "azure devops",
-    "azure functions",
-    "google compute engine",
-    "google app engine",
-    "firebase",
-    "cloud run",
-    "e3"
-  );
-
-  
 }
