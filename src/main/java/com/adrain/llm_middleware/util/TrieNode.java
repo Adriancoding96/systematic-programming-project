111907412fafab6d60762bfff92eb0d8856c08b9
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-02 14:53:09 +0000

    **Added** TrieNode for Trie data structure

---
+++ src/main/java/com/adrain/llm_middleware/util/TrieNode.java
@@ -0,0 +1,20 @@
+package com.adrain.llm_middleware.util;
+
+import java.util.HashMap;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TrieNode {
+
+  private HashMap<Character, TrieNode> children;
+  private String content;
+  private boolean isWord;
+  
+}
