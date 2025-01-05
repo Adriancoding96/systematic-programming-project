0ba71b921f3fe435ce6e27e39be55bbbcd64a417
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-05 13:11:34 +0000

    **Added** mapper class to map between response pojos and records

--- src/main/java/com/adrain/llm_middleware/mapper/ResponseMapper.java
+++ src/main/java/com/adrain/llm_middleware/mapper/ResponseMapper.java
@@ -3,14 +3,25 @@ package com.adrain.llm_middleware.mapper;
 import com.adrain.llm_middleware.model.Response;
 import com.adrain.llm_middleware.record.response.ResponseRecord;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
-@Mapper
-public interface ResponseMapper {
+@Component
+public class ResponseMapper {
 
-  ResponseMapper INSTANCE = Mappers.getMapper(ResponseMapper.class);
-  ResponseRecord mapResponseToRecord(Response response);
-  Response mapRecordToResponse(ResponseRecord record);
-  
+  public Response toResponse(ResponseRecord record) {
+    return new Response(
+        null,
+        null,
+        record.responseBody(),
+        record.metaData(),
+        record.rating());
+  }
+
+  public ResponseRecord toRecord(Response response) {
+    return new ResponseRecord(
+        response.getResponseBody(),
+        response.getMetaData(),
+        response.getRating());
+  }
+
 }
