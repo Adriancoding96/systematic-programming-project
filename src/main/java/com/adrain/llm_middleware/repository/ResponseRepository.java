277683162313250fdba7b9b7397079cf6f8c9d00
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-13 17:01:37 +0000

    **Added** method in ResponseRepository to fetch all responses by user email

--- src/main/java/com/adrain/llm_middleware/repository/ResponseRepository.java
+++ src/main/java/com/adrain/llm_middleware/repository/ResponseRepository.java
@@ -1,7 +1,12 @@
 package com.adrain.llm_middleware.repository;
 
+import java.util.List;
+
 import com.adrain.llm_middleware.model.Response;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResponseRepository extends JpaRepository<Response, Long> {}
+public interface ResponseRepository extends JpaRepository<Response, Long> {
+
+  List<Response> findAllByUserEmail(String email);
+}
