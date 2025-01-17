a96df621c4c02777197dfc1e3beaf46ef8ef0aec
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-17 14:57:33 +0000

    **Remoced** KeywordSearcherTests class. **Added** documentation to PromptResposiory

--- src/main/java/com/adrain/llm_middleware/repository/PromptRepository.java
+++ src/main/java/com/adrain/llm_middleware/repository/PromptRepository.java
@@ -7,8 +7,24 @@ import com.adrain.llm_middleware.model.Prompt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+/**
+ * PromptRepository is a repository interface for managing {@link Prompt} entities in the database.
+ * <p>
+ * Provides methods to perform CRUD operations and custom queries on {@code Prompt} objects.
+ * </p>
+ */
 public interface PromptRepository extends JpaRepository<Prompt, Long> {
 
+  /**
+   * Retrieves all {@link Prompt} entities associated with a user email.
+   * <p>
+   *     The query selects all prompts where the {@code user.email} matches
+   *     the specified {@code email}.
+   * </p>
+   *
+   * @param email The email of the user whose prompts are to be fetched.
+   * @return A list of {@code Prompt} entities matching the users email.
+   */
   @Query("SELECT p FROM Prompt p WHERE p.user.email = :email")
   List<Prompt> findAllByUserEmail(String email);
 
--- src/test/java/com/adrain/llm_middleware/KeywordSearcherTests.java
+++
@@ -1,8 +0,0 @@
-package com.adrain.llm_middleware;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class KeywordSearcherTests {
-
-}
