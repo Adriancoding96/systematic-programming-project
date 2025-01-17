902f092b9d3541edbe7bb0907b906cda049bcad1
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-17 13:07:03 +0000

    **Added** documentation to ResponseMapperTest

--- src/test/java/com/adrain/llm_middleware/ResponseMapperTest.java
+++ src/test/java/com/adrain/llm_middleware/ResponseMapperTest.java
@@ -15,6 +15,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * ResponseMapperTest is a test class to verify the correct functionality
+ * of the {@link ResponseMapper} class.
+ */
 @SpringBootTest
 public class ResponseMapperTest {
 
@@ -25,6 +29,17 @@ public class ResponseMapperTest {
     this.mapper = new ResponseMapper();
   }
 
+  /**
+   * Tests that the {@link ResponseMapper} can map a {@link Response} entity to a
+   * {@link ResponseRecord}.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The resulting record is not {@code null}.</li>
+   *   <li>The fields match the expected values from the {@code Response} entity.</li>
+   * </ul>
+   */
   @Test
   void testMapResponseToRecord() {
     Response response = new Response();
@@ -42,6 +57,17 @@ public class ResponseMapperTest {
     assertEquals(ResponseRating.VERY_USEFUL, record.rating());
   }
 
+  /**
+   * Tests that the {@link ResponseMapper} can map a {@link ResponseRecord} to a
+   * {@link Response} entity.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The resulting response is not {@code null}.</li>
+   *   <li>The fields match the expected values from the {@code ResponseRecord}.</li>
+   * </ul>
+   */
   @Test
   void testMapRecordToResponse() {
     ResponseRecord record = new ResponseRecord(
