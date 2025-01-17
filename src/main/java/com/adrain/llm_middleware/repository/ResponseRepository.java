c1adb2778a3826130c62f113070016384e40cb71
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-17 12:04:27 +0000

    **Added** getAllResponsesByUserEmail query in ResponseRepotsitory

--- src/main/java/com/adrain/llm_middleware/repository/ResponseRepository.java
+++ src/main/java/com/adrain/llm_middleware/repository/ResponseRepository.java
@@ -1,14 +1,15 @@
 package com.adrain.llm_middleware.repository;
 
+import java.util.List;
+
 import com.adrain.llm_middleware.model.Response;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ResponseRepository extends JpaRepository<Response, Long> {
 
-  /*
   @Query("SELECT r FROM Response r WHERE r.user.email = :email")
   List<Response> findAllByUserEmail(String email);
-  */
 
 }
