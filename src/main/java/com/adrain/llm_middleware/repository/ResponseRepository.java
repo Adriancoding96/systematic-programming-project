455288e155d6444402a273cea55cc6b0ea49fe2a
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-17 10:32:54 +0000

    **Changed** temporarly removed getAllResponsesByEmail query due to missing relational mappings

--- src/main/java/com/adrain/llm_middleware/repository/ResponseRepository.java
+++ src/main/java/com/adrain/llm_middleware/repository/ResponseRepository.java
@@ -1,12 +1,14 @@
 package com.adrain.llm_middleware.repository;
 
-import java.util.List;
-
 import com.adrain.llm_middleware.model.Response;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResponseRepository extends JpaRepository<Response, Long> {
 
+  /*
+  @Query("SELECT r FROM Response r WHERE r.user.email = :email")
   List<Response> findAllByUserEmail(String email);
+  */
+
 }
