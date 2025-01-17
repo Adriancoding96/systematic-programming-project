9fab41a1c11dbd020f7ad57d37ae77475e5423f2
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-17 10:59:31 +0000

    **Added*** OneToMany mapping to Response model

--- src/main/java/com/adrain/llm_middleware/model/User.java
+++ src/main/java/com/adrain/llm_middleware/model/User.java
@@ -1,9 +1,12 @@
 package com.adrain.llm_middleware.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -25,7 +28,13 @@ public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
+  
   private String email;
+  
   private String password;
+
+  @OneToMany(mappedBy = "user")
+  private List<Response> responses; 
 }
