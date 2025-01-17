5fd04c9279b34357eb78476f68d57b9c9f4d2340
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-17 15:20:16 +0000

    **Added** documentation for AhoCorasickTrie

--- src/main/java/com/adrain/llm_middleware/util/AhoCorasickTrie.java
+++ src/main/java/com/adrain/llm_middleware/util/AhoCorasickTrie.java
@@ -8,15 +8,33 @@ import java.util.Queue;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * AhoCorasickTrie is an implementation of the Aho-Corasick string matching algorithm,
+ * extending the base {@link Trie} class. It provides efficient pattern matching for
+ * multiple keywords simultaneously.
+ * <p>
+ *     This implementation maintains a {@link AhoCorasickNode} as the root node,
+ *     constructs failure links to navigate on mismatches, and provides methods to
+ *     insert keywords and search for them in a given text.
+ * </p>
+ */
 @Getter
 @Setter
 public class AhoCorasickTrie extends Trie {
 
+  /**
+   * Constructs an AhoCorasickTrie by setting the root node to {@link AhoCorasickNode}.
+   */
   public AhoCorasickTrie() {
     super();
     this.setRoot(new AhoCorasickNode());
   }
 
+  /**
+   * Inserts a list of words into this trie.
+   *
+   * @param words The list of words to insert.
+   */
   @Override
   public void insertAll(List<String> words) {
     for(String word : words) {
@@ -24,6 +42,11 @@ public class AhoCorasickTrie extends Trie {
     }
   }
 
+  /**
+   * Inserts a single word into this trie.
+   *
+   * @param word The word to insert.
+   */
   @Override
   public void insert(String word) {
     AhoCorasickNode current = (AhoCorasickNode) getRoot();
@@ -41,6 +64,13 @@ public class AhoCorasickTrie extends Trie {
     current.getOutput().add(word);
   }
 
+  /**
+   * Builds the failure links for the trie using a breadth-first search.
+   * <p>
+   *     Failure links allow the algorithm to efficiently transition to the next
+   *     possible matching state when a character does not match the current node.
+   * </p>
+   */
   public void buildFailureLinks() {
     Queue<AhoCorasickNode> queue = new LinkedList<>();
     AhoCorasickNode root = (AhoCorasickNode) getRoot();
@@ -83,6 +113,16 @@ public class AhoCorasickTrie extends Trie {
     }
   }
 
+  /**
+   * Searches for all keywords in the given text.
+   * <p>
+   *     The search uses the failure links to transition between nodes when
+   *     characters dont match.
+   * </p>
+   *
+   * @param text The text in which to search for the inserted keywords.
+   * @return A list of matched keywords found in the text.
+   */
   public List<String> searchText(String text) {
     AhoCorasickNode current = (AhoCorasickNode) getRoot();
     List<String> result = new ArrayList<>();
