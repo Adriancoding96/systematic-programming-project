b4352d87a84792f9bcf2e409bc93fb3d15f12671
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-20 21:21:47 +0000

    **Added** method declaration in ResponseService to get response by prompt id

--- src/main/java/com/adrain/llm_middleware/service/ResponseService.java
+++ src/main/java/com/adrain/llm_middleware/service/ResponseService.java
@@ -2,11 +2,13 @@ package com.adrain.llm_middleware.service;
 
 import java.util.List;
 
+import com.adrain.llm_middleware.model.Response;
 import com.adrain.llm_middleware.record.response.ResponseRecord;
 
 public interface ResponseService {
   void newResponse(ResponseRecord record);
   List<ResponseRecord> getAllResponsesByUserEmail();
   ResponseRecord getResponseById(Long id);
+  Response getResponseByPromptId(Long id);
   void deleteResponseById(Long id);
 }
