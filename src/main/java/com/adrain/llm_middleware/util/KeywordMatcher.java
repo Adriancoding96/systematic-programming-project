2d055dd0fffe86c09cfac0f3cdb151ca2bf0e53f
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-20 18:36:10 +0000

    **Added** KeywordMatcher class for comparing Strings against streams of prompts

--- src/main/java/com/adrain/llm_middleware/util/KeywordMatcher.java
+++ src/main/java/com/adrain/llm_middleware/util/KeywordMatcher.java
@@ -10,6 +10,13 @@ import com.adrain.llm_middleware.model.Prompt;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * KeywordMatcher is an component for comparing text similarity and building word frequency maps.
+ * <p>
+ *     It uses regular expressions to analyze text and calculate similarity scores between
+ *     a given input text and a collection of {@link Prompt} objects.
+ * </p>
+ */
 @Component
 public class KeywordMatcher {
 
@@ -20,6 +27,17 @@ public class KeywordMatcher {
     pattern = Pattern.compile(REGEX);
   }
   
+  /**
+   * Checks similarity between an input text and a stream of {@link Prompt} objects.
+   * <p>
+   *     Compares the input text against each {@link Prompt} in the stream by calculating a similarity
+   *     score. If the similarity is >= to 0.8 the matching {@link Prompt} is returned.
+   * </p>
+   *
+   * @param text The input text to compare.
+   * @param promptRows A stream of {@link Prompt} objects to compare against.
+   * @return The first {@link Prompt} object with a similarity >= 0.8 or {@code null} if no match is found.
+   */
   public Prompt checkSimilarityOfTextAndStream(String text, Stream<Prompt> promptRows) {
     Map<String, Integer> inputWordFreq = buildWordFrequencyMap(text);
 
@@ -38,7 +56,16 @@ public class KeywordMatcher {
     }
     return wordFreq;
   }
-
+ 
+  /**
+   * Builds a word frequency map from a given text.
+   * <p>
+   *     Extracts words matching the regex pattern and counts their occurrences in the text.
+   * </p>
+   *
+   * @param text The text to process.
+   * @return A {@link Map} where keys are words and values are their respective frequencies.
+   */
   public double calculateSimilarity(Map<String, Integer> inputWordFreq, String prompt) {
     Map<String, Integer> promptWordFreq = new HashMap<>();
     Matcher matcher = pattern.matcher(prompt);
---
+++ src/test/java/com/adrain/llm_middleware/KeywordMatcherTest.java
@@ -0,0 +1,191 @@
+package com.adrain.llm_middleware;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+
+import com.adrain.llm_middleware.model.Prompt;
+import com.adrain.llm_middleware.util.KeywordMatcher;
+import com.adrain.llm_middleware.util.KeywordSearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * KeywordMatcherTest is to verify correct functionality of {@link KeywordMatcher}
+ * methods.
+ */
+@SpringBootTest
+@ActiveProfiles("test")
+public class KeywordMatcherTest {
+
+  private final KeywordMatcher matcher = new KeywordMatcher();
+
+  /**
+   * Tests that the {@link KeywordSearcher#buildWordFrequencyMap} can construct
+   * a hashmap containing words and their frequency from a text.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The resulting hashmap is equal to the hardcoded example.</li>
+   * </ul>
+   */
+  @Test
+  void testBuildWordFrequencyMap() {
+    String text = "Hello World, Hello World.";
+    Map<String, Integer> expected = Map.of(
+      "Hello", 2,
+      "World", 2
+    );
+
+    Map<String, Integer> result = matcher.buildWordFrequencyMap(text);
+    assertEquals(expected, result);
+  }
+
+  /**
+   * Tests that the {@link KeywordSearcher#calculateSimilarityOfStrings} produces the
+   * correct similarity score of two identical strings.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The resulting similarity score is >= 0.8</li>
+   * </ul>
+   */
+  @Test
+  void testCalculateSimilarityOfEqualStrings() {
+    String prompt = "how do i deep copy a struct in rust";
+    Map<String, Integer> inputWordFreq = Map.of(
+      "how", 1,
+      "do", 1,
+      "i", 1,
+      "deep", 1,
+      "copy", 1,
+      "a", 1,
+      "struct", 1,
+      "in", 1,
+      "rust", 1
+    );
+
+    double similarity = matcher.calculateSimilarity(inputWordFreq, prompt);
+    assertTrue(similarity >= 0.8, "Expected similarity to be >= 0,8");
+  }  
+ 
+  /**
+   * Tests that the {@link KeywordSearcher#calculateSimilarityOfStrings} produces the
+   * correct similarity score for two completly different strings.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The resulting similarity score is <= 0.8</li>
+   * </ul>
+   */
+  @Test
+  void testCalculateSimilarityOfNotEqualStrings() {
+    String prompt = "how do i deep copy a struct in rust";
+    Map<String, Integer> inputWordFreq = Map.of(
+      "how", 1,
+      "do", 1,
+      "i", 1,
+      "center", 1,
+      "a", 1,
+      "div", 1,
+      "in", 1,
+      "html", 1,
+      "please", 1
+    );
+
+    double similarity = matcher.calculateSimilarity(inputWordFreq, prompt);
+    assertFalse(similarity >= 0.8, "Expected similarity to be >= 0,8");
+  }
+
+  /**
+   * Tests that the {@link KeywordSearcher#calculateSimilarityOfStrings} produces the
+   * correct similarity score for two closely resembling strings.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The resulting similarity score is >= 0.8</li>
+   * </ul>
+   */
+  @Test
+  void testCalculateSimilarityOfSimilarStrings() {
+    String prompt = "how do i deep copy a struct in rust";
+    Map<String, Integer> inputWordFreq = Map.of(
+      "how", 1,
+      "do", 1,
+      "i", 1, // Notice the word deep is not present in this test
+      "copy", 1,
+      "a", 1,
+      "struct", 1,
+      "in", 1,
+      "rust", 1
+    );
+
+    double similarity = matcher.calculateSimilarity(inputWordFreq, prompt);
+    assertTrue(similarity >= 0.8, "Expected similarity to be >= 0,8. The similarity was: " + similarity);
+  }
+
+  /**
+   * Tests that the {@link KeywordSearcher#checkSimilarityOfTextAndStream} returns a {@link Prompt} from a {@code Stream} 
+   * that has a similarity score of 0.8 or greater compared to the provided text.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The {@link Prompt} result is not {@code null}.</li>
+   *   <li>The {@link Prompt} equals the first possble occurance with a similarity score >= 0.8.</li>
+   * </ul>
+   */
+  @Test
+  void testCheckSimilarityOfTextAndStream() {
+    String prompt = "How do i center a div in html using css";
+    List<Prompt> prompts = List.of(
+      new Prompt(null, "I like programming in java", null, null),
+      new Prompt(null, "How", null, null),
+      new Prompt(null, "How do", null, null),
+      new Prompt(null, "How do i", null, null),
+      new Prompt(null, "How do i center", null, null),
+      new Prompt(null, "How do i center a", null, null),
+      new Prompt(null, "How do i center a div in", null, null),
+      new Prompt(null, "How do i center a div in html", null, null), // This should be present in result as 8 of 10 words match
+      new Prompt(null, "How do i center a div in html using", null, null),
+      new Prompt(null, "How do i center a div in html using css", null, null)
+    );
+
+    Prompt result = matcher.checkSimilarityOfTextAndStream(prompt, prompts.stream());
+    assertNotNull(result);
+    assertEquals("How do i center a div in html", result.getPrompt());
+  }
+
+  /**
+   * Tests that the {@link KeywordSearcher#checkSimilarityOfTextAndStream} returns null, as no {@link Prompt},s is
+   * equal to similarity score >= 0.8.
+   * <p>
+   *    Verifies that:
+   * </p>
+   * <ul>
+   *   <li>The {@link Prompt} result is {@code null}.</li>
+   * </ul>
+   */
+  @Test
+  void testCheckSimilarityOfTextAndStream_NoMatch() {
+    String prompt = "Java is fun!";
+    List prompts = List.of(
+      new Prompt(null, "How do i center a div in html using css", null, null),
+      new Prompt(null, "How do i deep copy a struct in rust", null, null)
+    );
+
+    Prompt result = matcher.checkSimilarityOfTextAndStream(prompt, prompts.stream());
+    assertNull(result);
+  }
+}
