25970844148d5f4ba2a4c17f992c3b9a5f3f639f
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-20 11:49:26 +0000

    **Added** PromptMapper class

---
+++ src/main/java/com/adrain/llm_middleware/mapper/PromptMapper.java
@@ -0,0 +1,29 @@
+package com.adrain.llm_middleware.mapper;
+
+import com.adrain.llm_middleware.model.Prompt;
+import com.adrain.llm_middleware.record.prompt.PromptRequest;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PromptMapper {
+
+ 
+  /**
+   * 
+   * Maps a {@link PromptRequest} to a {@link Prompt}.
+   * Currently only the prompt string is mapped as the response is
+   * not yet available, and the user is fethed through application context in
+   * the service implementation
+   *
+   * @param request the request from the end user
+   * @return the prompt mapped from the request
+   *
+   * */
+  public Prompt toPromptFromRequest(PromptRequest request) {
+    Prompt prompt = new Prompt();
+    prompt.setPrompt(request.prompt());
+    return prompt;
+  }
+  
+}
