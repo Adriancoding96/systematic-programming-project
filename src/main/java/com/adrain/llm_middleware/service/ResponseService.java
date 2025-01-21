f961fe106d25f7c2ad5bbca12e3cb440fb6988df
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-21 13:09:56 +0000

    **Added** method declaration for querying responses by email and response body in ResponseService

--- src/main/java/com/adrain/llm_middleware/service/ResponseService.java
+++ src/main/java/com/adrain/llm_middleware/service/ResponseService.java
@@ -8,6 +8,7 @@ import com.adrain.llm_middleware.record.response.ResponseRecord;
 public interface ResponseService {
   void newResponse(ResponseRecord record);
   List<ResponseRecord> getAllResponsesByUserEmail();
+  List<ResponseRecord> findResponsesByResponseBodyAndUserEmail(String responseBody);
   ResponseRecord getResponseById(Long id);
   Response getResponseByPromptId(Long id);
   void deleteResponseById(Long id);
