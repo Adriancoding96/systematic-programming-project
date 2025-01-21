d3ad1ecf6d6758a9fbdcdaacafe02e6fb9b22adb
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-21 14:50:21 +0000

    **Added** query to fetch prompt by uuid

--- src/main/java/com/adrain/llm_middleware/repository/PromptRepository.java
+++ src/main/java/com/adrain/llm_middleware/repository/PromptRepository.java
@@ -6,6 +6,7 @@ import com.adrain.llm_middleware.model.Prompt;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * PromptRepository is a repository interface for managing {@link Prompt} entities in the database.
@@ -26,5 +27,17 @@ public interface PromptRepository extends JpaRepository<Prompt, Long> {
    * @return A stream of {@code Prompt} entities matching the users email.
    */
   @Query("SELECT p FROM Prompt p WHERE p.user.email = :email")
-  Stream<Prompt> findAllByUserEmail(String email);
+  Stream<Prompt> findAllByUserEmail(@Param("email") String email);
+  
+  /**
+   * Retrieves {@link Prompt} with specified uuid.
+   * <p>
+   *     The query fetches {@link Prompt} with specified uuid.
+   * </p>
+   *
+   * @param uuid of database row.
+   * @return A {@link Prompt} entity matching the uuid.
+   */
+  @Query("SELECT p FROM Prompt p WHERE p.uuid = :uuid")
+  Stream<Prompt> findByUuid(@Param("uuid") String uuid);
 }
