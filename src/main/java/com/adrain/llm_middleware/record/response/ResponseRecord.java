8ede8e6cb2c2415c644bd955c0ce046d8c0fe101
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-22 16:51:57 +0000

    **Added** JsonProperty annotations to record attributes

--- src/main/java/com/adrain/llm_middleware/record/response/ResponseRecord.java
+++ src/main/java/com/adrain/llm_middleware/record/response/ResponseRecord.java
@@ -3,13 +3,14 @@ package com.adrain.llm_middleware.record.response;
 import java.util.List;
 
 import com.adrain.llm_middleware.enums.ResponseRating;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Builder;
 
 @Builder
 public record ResponseRecord(
-    String responseBody,
-    List<String> metaData,
-    ResponseRating rating,
-    String promptUuid
+    @JsonProperty("responseBody") String responseBody,
+    @JsonProperty("metaData") List<String> metaData,
+    @JsonProperty("rating") ResponseRating rating,
+    @JsonProperty("promptUuid") String promptUuid
 ) {}
