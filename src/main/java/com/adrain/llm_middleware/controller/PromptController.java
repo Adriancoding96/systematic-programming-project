2b8cc9998d1379563defa13094cbb35d6f0aa714
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 16:27:47 +0000

    **Added** javadocs for PromptController

--- src/main/java/com/adrain/llm_middleware/controller/PromptController.java
+++ src/main/java/com/adrain/llm_middleware/controller/PromptController.java
@@ -1,5 +1,7 @@
 package com.adrain.llm_middleware.controller;
 
+import com.adrain.llm_middleware.model.Prompt;
+import com.adrain.llm_middleware.record.prompt.PromptRecord;
 import com.adrain.llm_middleware.record.prompt.PromptRequest;
 import com.adrain.llm_middleware.record.prompt.PromptResponse;
 import com.adrain.llm_middleware.service.PromptService;
@@ -11,6 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller responsible for handling {@link Prompt} requests.
+ * This controller provides endpoints for creating, retrieving, searching, and deleting
+ * {@link Prompt}s.
+ *
+ * @see RestController
+ * @see PromptService 
+ * @see PromptRequest
+ * @see PromptRecord
+ */
 @RestController
 @RequestMapping("/api/prompt")
 public class PromptController {
@@ -22,9 +34,19 @@ public class PromptController {
     this.promptService = promptService;
   }
 
+
+  /**
+   * Handles requests to create a new {@link Prompt}.
+   *
+   * @param record the {@link Prompt} request record {@link PromptRequest} containig 
+   * {@link Prompt} data.
+   * @return a {@link PromptResponse} with HTTP status 200 (OK) upon successful creation
+   */
   @PostMapping("/new")
   public ResponseEntity<PromptResponse> newPrompt(@RequestBody PromptRequest request) {
     return ResponseEntity.ok(promptService.newPrompt(request)); 
   }
+
+  
 
 }
