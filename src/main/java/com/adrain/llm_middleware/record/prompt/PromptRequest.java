9753b56d1f3e0da05837c5b70f1b6c75ca821aa7
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 10:46:47 +0000

    **Added** javadocs for PromptRequest

--- src/main/java/com/adrain/llm_middleware/record/prompt/PromptRequest.java
+++ src/main/java/com/adrain/llm_middleware/record/prompt/PromptRequest.java
@@ -1,3 +1,16 @@
 package com.adrain.llm_middleware.record.prompt;
 
+import com.adrain.llm_middleware.model.Prompt;
+import com.adrain.llm_middleware.model.Response;
+
+/**
+ * Represents a request containing details of a {@link Prompt}.
+ *
+ * @param prompt the text prompt provided by the {@link User}
+ * @param model  the identifier of the model to be used for generating the {@link Response}
+ *
+ * @see Prompt
+ * @see User
+ * @see Response
+ */
 public record PromptRequest(String prompt, String model) {}
