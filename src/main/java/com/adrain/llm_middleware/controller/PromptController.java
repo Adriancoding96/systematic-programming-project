cb05fb34f309bf8b8d399114da43e8b4862dfa62
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 16:38:24 +0000

    **Added** endpoints for Prompt CRUD operations

--- src/main/java/com/adrain/llm_middleware/controller/PromptController.java
+++ src/main/java/com/adrain/llm_middleware/controller/PromptController.java
@@ -10,7 +10,9 @@ import com.adrain.llm_middleware.service.PromptService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,9 +52,35 @@ public class PromptController {
     return ResponseEntity.ok(promptService.newPrompt(request)); 
   }
 
+  /**
+   * Handles requests to fetch all {@link Prompt} entities from database.
+   *
+   * @return a list of {@link PromptRecord} with HTTP status 200 (OK) upon successful creation
+   */
   @GetMapping
   public ResponseEntity<List<PromptRecord>> getAllPrompts() {
     return ResponseEntity.ok(promptService.getAllPrompts());
+  }
+
+  /**
+   * Handles requests to fetch a {@link Prompt} by id.
+   *
+   * @return a {@link PromptRecord} with HTTP status 200 (OK) upon successful creation
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<PromptRecord> getPromptById(@PathVariable Long id) {
+    return ResponseEntity.ok(promptService.getPromptById(id));
+  }
+
+  /**
+   * Handles requests to delete a {@link Prompt} by id.
+   *
+   * @return a {@code Void} response with HTTP status 200 (OK) upon successful creation
+   */
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deletePromptById(@PathVariable Long id) {
+    promptService.deletePromptById(id);
+    return ResponseEntity.ok().build();
   }
 
   
