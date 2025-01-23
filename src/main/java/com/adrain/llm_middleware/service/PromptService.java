b1168799a7d56a917c859bbbe646a4fc4c385462
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 11:48:02 +0000

    **Added** javadocs for PromptService interface

--- src/main/java/com/adrain/llm_middleware/service/PromptService.java
+++ src/main/java/com/adrain/llm_middleware/service/PromptService.java
@@ -1,10 +1,25 @@
 package com.adrain.llm_middleware.service;
 
+import com.adrain.llm_middleware.model.Prompt;
 import com.adrain.llm_middleware.record.prompt.PromptRequest;
 import com.adrain.llm_middleware.record.prompt.PromptResponse;
 
+/**
+ * Service interface for handling {@link Prompt} related requests.
+ * This interface defines a method for creating a new {@link Prompt} based on the passed http request.
+ *
+ * @see PromptResponse
+ * @see PromptRequest
+ */
 public interface PromptService {
 
+
+  /**
+   * Creates a new {@link Prompt} based on the http request.
+   *
+   * @param request the request containing the details for the new {@link Prompt}
+   * @return the response containing the result of the {@link Prompt} creation
+   */
   PromptResponse newPrompt(PromptRequest request);
   
 }
