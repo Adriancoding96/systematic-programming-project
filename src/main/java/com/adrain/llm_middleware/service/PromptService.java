d6a74c8692d499901ef0ec687e6c65e48dbd990a
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 16:01:02 +0000

    **Added** method signatures to fetch all, fetch by id, and delete by id to PromptService interface

--- src/main/java/com/adrain/llm_middleware/service/PromptService.java
+++ src/main/java/com/adrain/llm_middleware/service/PromptService.java
@@ -1,8 +1,12 @@
 package com.adrain.llm_middleware.service;
 
+import java.util.List;
+
 import com.adrain.llm_middleware.model.Prompt;
+import com.adrain.llm_middleware.model.Response;
 import com.adrain.llm_middleware.record.prompt.PromptRequest;
 import com.adrain.llm_middleware.record.prompt.PromptResponse;
+import com.adrain.llm_middleware.record.response.ResponseRecord;
 
 /**
  * Service interface for handling {@link Prompt} related requests.
@@ -21,5 +25,25 @@ public interface PromptService {
    * @return the response containing the result of the {@link Prompt} creation
    */
   PromptResponse newPrompt(PromptRequest request);
+
+  /**
+   * Fetches all {@link Response}s as {@link ResponseRecord} from datavase.
+   *
+   * @return a list of {@link ResponseRecord}s mapped from {@link Response} entities.
+   */
+  List<PromptResponse> getAllResponses();
+
+  /**
+   * Fetches {@link Response} from the database by id amd returns it as {@link ResponseRecord}.
+   *
+   * @return a fethced {@link Response} as {@link ResponseRecord}.
+   */
+  ResponseRecord getResponseById(Long id);
+
+
+  /**
+   * Deletes {@link Response} from the database.
+   */
+  void deleteResponseById(Long id);
   
 }
