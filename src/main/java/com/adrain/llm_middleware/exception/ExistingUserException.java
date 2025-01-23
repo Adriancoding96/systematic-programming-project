428118e92fd5ce28ab685ff5cb79d378c92884b0
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 09:55:23 +0000

    **Added** javadocs to ExistingUserException

--- src/main/java/com/adrain/llm_middleware/exception/ExistingUserException.java
+++ src/main/java/com/adrain/llm_middleware/exception/ExistingUserException.java
@@ -1,5 +1,12 @@
 package com.adrain.llm_middleware.exception;
 
+/**
+ * Exception thrown when an request attempts to create or register a {@link User} that already exists.
+ * This exception extends {@link RuntimeException} and is used to indicate that a user with the same
+ * identifier already exists in the database.
+ *
+ * @see RuntimeException
+ */
 public class ExistingUserException extends RuntimeException {
 
   public ExistingUserException(String message) {
