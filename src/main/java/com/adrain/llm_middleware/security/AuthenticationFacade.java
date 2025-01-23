8980e85789bfae9324de08cf4f5a70efff7bda2f
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 11:15:48 +0000

    **Added** javadocs to AuthenticationFacade

--- src/main/java/com/adrain/llm_middleware/security/AuthenticationFacade.java
+++ src/main/java/com/adrain/llm_middleware/security/AuthenticationFacade.java
@@ -1,17 +1,30 @@
 package com.adrain.llm_middleware.security;
 
+import com.adrain.llm_middleware.model.User;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Component is responsible for giving access to the current {@link Authentication} object
+ * from the {@link SecurityContextHolder}.
+ *
+ * <p>It provides a method to retrieve the current {@link Authentication} object, which contains
+ * details about the currently authenticated {@link User}.</p>
+ *
+ * @see Authentication
+ * @see SecurityContextHolder
+ * @see User
+ */
 @Component
 public class AuthenticationFacade {
-  
-  /*
-   * Gets authentication data from security context
+ 
+  /**
+   * Retrieves the current {@link Authentication} object from the {@link SecurityContextHolder}.
    *
-   * @return authentication: Object containing auth data
-   * */
+   * @return the current {@link Authentication} object, or {@code null} if no authentication is available
+   */
   public Authentication getAuthentication() {
     return SecurityContextHolder.getContext().getAuthentication();
   }
