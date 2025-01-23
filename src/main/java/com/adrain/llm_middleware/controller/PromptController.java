bea163552af31d204429ed92cc19a570d3aea75b
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-23 13:55:08 +0000

    **Removed** /docs from gitignore

--- src/main/java/com/adrain/llm_middleware/controller/PromptController.java
+++ src/main/java/com/adrain/llm_middleware/controller/PromptController.java
@@ -1,5 +1,6 @@
 package com.adrain.llm_middleware.controller;
 
+import com.adrain.llm_middleware.model.Prompt;
 import com.adrain.llm_middleware.record.prompt.PromptRequest;
 import com.adrain.llm_middleware.record.prompt.PromptResponse;
 import com.adrain.llm_middleware.service.PromptService;
@@ -11,6 +12,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for handling {@link Prompt} requests.
+ * This controller provides endpoints for creating new prompts.
+ *
+ * @see RestController
+ * @see PromptService
+ * @see PromptRequest
+ * @see PromptResponse
+ */
 @RestController
 @RequestMapping("/api/prompt")
 public class PromptController {
@@ -22,6 +32,14 @@ public class PromptController {
     this.promptService = promptService;
   }
 
+  /**
+   * Handles requests to create a new {@link Prompt}.
+   * This method accepts a {@link PromptRequest} and 
+   * returning a {@link PromptResponse} on completion.
+   *
+   * @param request the request containing the prompt details
+   * @return a {@link ResponseEntity} containing the {@link PromptResponse} with the result
+   */
   @PostMapping("/new")
   public ResponseEntity<PromptResponse> newPrompt(@RequestBody PromptRequest request) {
     return ResponseEntity.ok(promptService.newPrompt(request)); 
