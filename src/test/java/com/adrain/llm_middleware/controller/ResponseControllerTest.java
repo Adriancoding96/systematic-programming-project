7a8b99aff9123e63a9510cd1aa771f149a471355
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-24 12:24:10 +0000

    **Added** unit test for #getResponseById in ResponseController

--- src/test/java/com/adrain/llm_middleware/controller/ResponseControllerTest.java
+++ src/test/java/com/adrain/llm_middleware/controller/ResponseControllerTest.java
@@ -1,6 +1,7 @@
 package com.adrain.llm_middleware.controller;
 
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -48,15 +49,14 @@ public class ResponseControllerTest {
   @BeforeEach
   public void setup() {
     mockMvc = MockMvcBuilders
-      .webAppContextSetup(webApplicationContext)
-      .apply(springSecurity())
-      .defaultRequest(get("/**").with(csrf().asHeader()))
-      .defaultRequest(post("/**").with(csrf().asHeader()))
-      .defaultRequest(put("/**").with(csrf().asHeader()))
-      .defaultRequest(delete("/**").with(csrf().asHeader()))
-      .build();
+        .webAppContextSetup(webApplicationContext)
+        .apply(springSecurity())
+        .defaultRequest(get("/**").with(csrf().asHeader()))
+        .defaultRequest(post("/**").with(csrf().asHeader()))
+        .defaultRequest(put("/**").with(csrf().asHeader()))
+        .defaultRequest(delete("/**").with(csrf().asHeader()))
+        .build();
   }
-
 
   @Test
   @WithMockUser
@@ -65,52 +65,165 @@ public class ResponseControllerTest {
         "You use html and css",
         List.of("html", "css"),
         ResponseRating.VERY_USEFUL,
-        "123456"
-    );
+        "123456");
     doNothing().when(responseService).newResponse(record);
     mockMvc.perform(post("/api/response/new"));
   }
 
- @Test
- @WithMockUser
- public void testGetAllResponses() throws Exception {
+  @Test
+  @WithMockUser
+  public void testGetAllResponses() throws Exception {
     ResponseRecord record1 = new ResponseRecord(
         "You use HTML and CSS",
         List.of("html", "css"),
         ResponseRating.VERY_USEFUL,
-        "123456"
-    );
+        "123456");
 
     ResponseRecord record2 = new ResponseRecord(
         "You can build a CLI with Rust and Tokio",
         List.of("rust", "tokio"),
         ResponseRating.VERY_USEFUL,
-        "123456"
-    );
+        "123456");
 
     ResponseRecord record3 = new ResponseRecord(
         "Java is a OOP language that sneaks in primitive types",
         List.of("java", "oop"),
         ResponseRating.VERY_USEFUL,
-        "123456"
-    );
+        "123456");
     List<ResponseRecord> records = List.of(record1, record2, record3);
 
     when(responseService.getAllResponses()).thenReturn(records);
     mockMvc.perform(get("/api/response"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].responseBody").value("You use HTML and CSS"))
+        .andExpect(jsonPath("$[0].metaData.size()").value(2))
+        // .andExpect(jsonPath("$[0].rating").value(ResponseRating.VERY_USEFUL))
+        .andExpect(jsonPath("$[0].promptUuid").value("123456"))
+        .andExpect(jsonPath("$[1].responseBody").value("You can build a CLI with Rust and Tokio"))
+        .andExpect(jsonPath("$[1].metaData.size()").value(2))
+        // .andExpect(jsonPath("$[1].rating").value(ResponseRating.VERY_USEFUL))
+        .andExpect(jsonPath("$[1].promptUuid").value("123456"))
+        .andExpect(jsonPath("$[2].responseBody").value("Java is a OOP language that sneaks in primitive types"))
+        .andExpect(jsonPath("$[2].metaData.size()").value(2))
+        // .andExpect(jsonPath("$[2].rating").value(ResponseRating.VERY_USEFUL))
+        .andExpect(jsonPath("$[2].promptUuid").value("123456"));
+
+  }
+
+  @Test
+  @WithMockUser
+  public void testGetAllResponsesByUserEmail() throws Exception {
+    ResponseRecord record1 = new ResponseRecord(
+        "You use HTML and CSS",
+        List.of("html", "css"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+
+    ResponseRecord record2 = new ResponseRecord(
+        "You can build a CLI with Rust and Tokio",
+        List.of("rust", "tokio"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+
+    ResponseRecord record3 = new ResponseRecord("Java is a OOP language that sneaks in primitive types",
+        List.of("java", "oop"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+    List<ResponseRecord> records = List.of(record1, record2, record3);
+
+    when(responseService.getAllResponsesByUserEmail()).thenReturn(records);
+    mockMvc.perform(get("/api/response/user"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].responseBody").value("You use HTML and CSS"))
+        .andExpect(jsonPath("$[0].metaData.size()").value(2))
+        // .andExpect(jsonPath("$[0].rating").value(ResponseRating.VERY_USEFUL))
+        .andExpect(jsonPath("$[0].promptUuid").value("123456"))
+        .andExpect(jsonPath("$[1].responseBody").value("You can build a CLI with Rust and Tokio"))
+        .andExpect(jsonPath("$[1].metaData.size()").value(2))
+        // .andExpect(jsonPath("$[1].rating").value(ResponseRating.VERY_USEFUL))
+        .andExpect(jsonPath("$[1].promptUuid").value("123456"))
+        .andExpect(jsonPath("$[2].responseBody").value("Java is a OOP language that sneaks in primitive types"))
+        .andExpect(jsonPath("$[2].metaData.size()").value(2))
+        // .andExpect(jsonPath("$[2].rating").value(ResponseRating.VERY_USEFUL))
+        .andExpect(jsonPath("$[2].promptUuid").value("123456"));
+  }
+
+  @Test
+  @WithMockUser()
+  public void testSearchResponses() throws Exception {
+    String query1 = "Java";
+    String query2 = "Rust";
+    String query3 = "HTML";
+    String query4 = "You";
+
+    ResponseRecord record1 = new ResponseRecord(
+        "You use HTML and CSS",
+        List.of("html", "css"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+
+    ResponseRecord record2 = new ResponseRecord(
+        "You can build a CLI with Rust and Tokio",
+        List.of("rust", "tokio"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+
+    ResponseRecord record3 = new ResponseRecord(
+        "Java is a OOP language that sneaks in primitive types",
+        List.of("java", "oop"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+
+    when(responseService.findResponsesByResponseBodyAndUserEmail(query1))
+        .thenReturn(List.of(record3));
+    when(responseService.findResponsesByResponseBodyAndUserEmail(query2))
+        .thenReturn(List.of(record2));
+    when(responseService.findResponsesByResponseBodyAndUserEmail(query3))
+        .thenReturn(List.of(record1));
+    when(responseService.findResponsesByResponseBodyAndUserEmail(query4))
+        .thenReturn(List.of(record1, record2));
+
+    mockMvc.perform(get("/api/response/search?query=Java"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(1))
+        .andExpect(jsonPath("$[0].responseBody").value("Java is a OOP language that sneaks in primitive types"));
+
+    mockMvc.perform(get("/api/response/search?query=Rust"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(1))
+        .andExpect(jsonPath("$[0].responseBody").value("You can build a CLI with Rust and Tokio"));
+
+    mockMvc.perform(get("/api/response/search?query=HTML"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(1))
+        .andExpect(jsonPath("$[0].responseBody").value("You use HTML and CSS"));
+
+    mockMvc.perform(get("/api/response/search?query=You"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(2))
+        .andExpect(jsonPath("$[0].responseBody").value("You use HTML and CSS"))
+        .andExpect(jsonPath("$[1].responseBody").value("You can build a CLI with Rust and Tokio"));
+
+    verify(responseService).findResponsesByResponseBodyAndUserEmail(query1);
+    verify(responseService).findResponsesByResponseBodyAndUserEmail(query2);
+    verify(responseService).findResponsesByResponseBodyAndUserEmail(query3);
+    verify(responseService).findResponsesByResponseBodyAndUserEmail(query4);
+  }
+
+  @Test
+  @WithMockUser
+  public void testGetResponseById() throws Exception {
+    ResponseRecord record = new ResponseRecord(
+        "You use HTML and CSS",
+        List.of("html", "css"),
+        ResponseRating.VERY_USEFUL,
+        "123456");
+
+    when(responseService.getResponseById(1L)).thenReturn(record);
+    mockMvc.perform(get("/api/response/{id}", 1L))
       .andExpect(status().isOk())
-      .andExpect(jsonPath("$[0].responseBody").value("You use HTML and CSS"))
-      .andExpect(jsonPath("$[0].metaData.size()").value(2))
-      //.andExpect(jsonPath("$[0].rating").value(ResponseRating.VERY_USEFUL))
-      .andExpect(jsonPath("$[0].promptUuid").value("123456"))
-      .andExpect(jsonPath("$[1].responseBody").value("You can build a CLI with Rust and Tokio"))
-      .andExpect(jsonPath("$[1].metaData.size()").value(2))
-      //.andExpect(jsonPath("$[1].rating").value(ResponseRating.VERY_USEFUL))
-      .andExpect(jsonPath("$[1].promptUuid").value("123456"))
-      .andExpect(jsonPath("$[2].responseBody").value("Java is a OOP language that sneaks in primitive types"))
-      .andExpect(jsonPath("$[2].metaData.size()").value(2))
-      //.andExpect(jsonPath("$[2].rating").value(ResponseRating.VERY_USEFUL))
-      .andExpect(jsonPath("$[2].promptUuid").value("123456"));
-  
- }
+      .andExpect(jsonPath("$.responseBody").value("You use HTML and CSS"))
+      .andExpect(jsonPath("$.metaData.size()").value(2));
+  }
+
 }
