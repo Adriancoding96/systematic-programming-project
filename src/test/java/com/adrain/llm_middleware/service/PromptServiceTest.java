6276e99e45343e97bd762a7354cd80b82d8595cb
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-26 12:13:48 +0000

    **Added** unit test for #getAllPrompts in PromptService

--- src/test/java/com/adrain/llm_middleware/service/PromptServiceTest.java
+++ src/test/java/com/adrain/llm_middleware/service/PromptServiceTest.java
@@ -1,55 +1,114 @@
 package com.adrain.llm_middleware.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 import com.adrain.llm_middleware.api.OpenAiClient;
+import com.adrain.llm_middleware.mapper.PromptMapper;
+import com.adrain.llm_middleware.model.Prompt;
+import com.adrain.llm_middleware.model.Response;
+import com.adrain.llm_middleware.model.User;
+import com.adrain.llm_middleware.record.prompt.PromptRecord;
+import com.adrain.llm_middleware.record.prompt.PromptRequest;
+import com.adrain.llm_middleware.record.prompt.PromptResponse;
 import com.adrain.llm_middleware.repository.PromptRepository;
 import com.adrain.llm_middleware.service.impl.PromptServiceImpl;
+import com.adrain.llm_middleware.util.KeywordMatcher;
+import com.adrain.llm_middleware.util.KeywordSearcher;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-
-@ActiveProfiles("test")
-public class PromptServiceTest {
+@ExtendWith(MockitoExtension.class)
+class PromptServiceTest {
 
   @Mock
   private PromptRepository promptRepository;
-
   @Mock
   private OpenAiClient openAiClient;
+  @Mock
+  private KeywordSearcher keywordSearcher;
+  @Mock
+  private PromptMapper promptMapper;
+  @Mock
+  private UserService userService;
+  @Mock
+  private KeywordMatcher keywordMatcher;
+  @Mock
+  private ResponseService responseService;
 
   @InjectMocks
-  private PromptServiceImpl promptServiceImpl;
+  private PromptServiceImpl promptService;
 
   @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
+  void setupSecurityContext() {
+    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken("adrian@example.com", null,
+        List.of());
+    SecurityContextHolder.getContext().setAuthentication(auth);
   }
 
-  /*
   @Test
-  void testNewPrompt() {
-    
-    PromptRequest request = new PromptRequest("How do i center a div in html?", "gpt-o1");
+  public void testNewPrompt_whenSimilarPromptExistsInDatabase() {
+    PromptRequest request = new PromptRequest("How do i not cause stack overflow???", "gpt-3.5-turbo");
 
-    OpenAiResponse.Choice choice = new OpenAiResponse.Choice("You use css LOL", 0, "stop");
+    Prompt prompt = new Prompt();
+    prompt.setPrompt("How do i not cause stack overflow???");
+    prompt.setUuid("12345");
 
-    OpenAiResponse openAiResponse = new OpenAiResponse(
-      "test-id",
-      "gpt-o1",
-      List.of(choice),
-      null
-    );
+    User user = new User();
+    user.setName("Adrian");
+    user.setEmail("adrian@example.com");
+    user.setPassword("verysecure123");
 
-    Mockito.when(openAiClient.getCompletion(request.prompt()))
-      .thenReturn(Mono.just(openAiResponse));
+    Response response = new Response();
+    response.setResponseBody("The stack can't stack that high");
+    response.setMetaData(List.of("stack"));
 
-    PromptResponse response = promptServiceImpl.newPrompt(request);
-    
-    Assertions.assertEquals("You use css LOL", response.response());
-    Mockito.verify(openAiClient).getCompletion("How do i center a div in html?");
-  }*/
+    when(userService.getUserBySecurityContext()).thenReturn(user);
+    when(promptMapper.toPromptFromRequest(request)).thenReturn(prompt);
+    when(promptRepository.findAllByUserEmail(user.getEmail())).thenReturn(Stream.of(prompt));
+    when(keywordMatcher.checkSimilarityOfTextAndStream(anyString(), any())).thenReturn(prompt);
+    when(responseService.getResponseByPromptId(null)).thenReturn(response);
+
+    PromptResponse result = promptService.newPrompt(request);
+
+    assertNotNull(result, "Expected non-null result from newPrompt");
+  }
+
+  @Test
+  public void testGetAllPrompts() {
+    Prompt prompt1 = new Prompt();
+    prompt1.setPrompt("How do i write unit tests faster?");
+    prompt1.setUuid("12345");
+
+    Prompt prompt2 = new Prompt();
+    prompt2.setPrompt("How do i run unit tests faster?");
+    prompt1.setUuid("54321");
+
+    when(promptRepository.findAll()).thenReturn(List.of(prompt1, prompt2));
+    when(promptMapper.toRecordFromPrompt(prompt1)).thenReturn(new PromptRecord("How do i write unit tests faster?", "12345"));
+    when(promptMapper.toRecordFromPrompt(prompt2)).thenReturn(new PromptRecord("How do i run unit tests faster?", "54321"));
+
+    List<PromptRecord> result = promptService.getAllPrompts();
+    assertNotNull(result);
+    assertEquals(2, result.size());
+    assertEquals("How do i write unit tests faster?", result.get(0).prompt());
+    assertEquals("12345", result.get(0).uuid());
+    assertEquals("How do i run unit tests faster?", result.get(1).prompt());
+    assertEquals("54321", result.get(1).uuid());
+
+  }
 
 }
