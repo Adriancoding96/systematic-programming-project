c7f6259b4eb77ae338c510d5d79442e246002a89
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-30 09:04:15 +0000

    **Added** endpoint to update prompt

--- src/main/java/com/adrain/llm_middleware/controller/PromptController.java
+++ src/main/java/com/adrain/llm_middleware/controller/PromptController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,6 +81,19 @@ public class PromptController {
   @GetMapping("/{id}")
   public ResponseEntity<PromptRecord> getPromptById(@PathVariable Long id) {
     return ResponseEntity.ok(promptService.getPromptById(id));
+  }
+
+  /**
+   * Handles requests to update a {@link Prompt} by id and {@link PromptRecord}.
+   *
+   * @param id the id of the existing {@link Prompt}
+   * @param record the dto containing update data
+   * @return a HTTP status 200 (OK) upon successful update
+   */
+  @PutMapping("/{id}")
+  public ResponseEntity<Void> updatePrompt(@PathVariable Long id, @RequestBody PromptRecord record) {
+    promptService.updatePrompt(id, record);
+    return ResponseEntity.ok().build();
   }
 
   /**
